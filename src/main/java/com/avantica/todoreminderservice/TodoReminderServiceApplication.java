1072b967429caf4ed9c35252e876ab7d685cf0a4
ArturoJulca-minted <arturo.julca@minted.com>
2019-03-12 17:54:20 -0500

    First commit

---
+++ src/main/java/com/avantica/todoreminderservice/TodoReminderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.avantica.todoreminderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoReminderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoReminderServiceApplication.class, args);
+	}
+
+}
