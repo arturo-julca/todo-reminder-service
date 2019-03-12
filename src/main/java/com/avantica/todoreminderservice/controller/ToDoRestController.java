8d8361c6adc0879dd96177983f10271ccef6347d
ArturoJulca-minted <arturo.julca@minted.com>
2019-03-12 18:21:47 -0500

    Added ToDo management

---
+++ src/main/java/com/avantica/todoreminderservice/controller/ToDoRestController.java
@@ -0,0 +1,67 @@
+package com.avantica.todoreminderservice.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.avantica.todoreminderservice.model.ToDo;
+import com.avantica.todoreminderservice.repository.ToDoRepository;
+
+
+@RestController
+@RequestMapping({"/todo"})
+public class ToDoRestController {
+
+	private ToDoRepository toDoRepository;
+	
+	ToDoRestController(ToDoRepository toDoRepository){
+		this.toDoRepository = toDoRepository;
+	}
+	
+	@GetMapping
+    public List findAll(){
+        return toDoRepository.findAll();
+    }
+
+    @GetMapping(path = {"/{id}"})
+    public ResponseEntity<ToDo> findById(@PathVariable long id){
+    	return toDoRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
+    }
+    
+    @PostMapping
+    public ToDo create(@RequestBody ToDo toDo){
+        return toDoRepository.save(toDo);
+    }
+    
+    @PutMapping(value="/{id}")
+    public ResponseEntity<ToDo> update(@PathVariable("id") long id,
+                                          @RequestBody ToDo contact){
+      return toDoRepository.findById(id)
+          .map(record -> {
+              record.setName(contact.getName());
+              record.setEmail(contact.getEmail());
+              ToDo updated = toDoRepository.save(record);
+              return ResponseEntity.ok().body(updated);
+          }).orElse(ResponseEntity.notFound().build());
+    }
+    
+    @DeleteMapping(path ={"/{id}"})
+    public ResponseEntity<?> delete(@PathVariable("id") long id) {
+      return toDoRepository.findById(id)
+          .map(record -> {
+        	  toDoRepository.deleteById(id);
+              return ResponseEntity.ok().build();
+          }).orElse(ResponseEntity.notFound().build());
+    }
+
+}
---
+++ src/main/java/com/avantica/todoreminderservice/model/ToDo.java
@@ -0,0 +1,62 @@
+package com.avantica.todoreminderservice.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+public class ToDo {
+	
+    @Id
+    @GeneratedValue
+	private Long id;
+	private String name;
+	@DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private LocalDateTime time;
+	private String email;
+	
+	public ToDo(){		
+	}
+	
+	public ToDo(Long id, String name, LocalDateTime time, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.time = time;
+		this.email = email;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LocalDateTime getTime() {
+		return time;
+	}
+	public void setTime(LocalDateTime time) {
+		this.time = time;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public String toString() {
+		return "ToDo [id=" + id + ", name=" + name + ", time=" + time + ", email=" + email + "]";
+	}
+	
+	
+}
